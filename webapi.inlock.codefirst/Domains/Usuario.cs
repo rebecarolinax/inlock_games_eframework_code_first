39adb270fcd5385c4ee1bc996120f6dfffe09bfb
Rebeca Carolina <@rebecacarolina75@gmail.com>
2023-09-13 17:18:15 -0300

    InLock EFramework

---
+++ webapi.inlock.codefirst/Domains/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.inlock.codefirst.Domains
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public Guid IdUsuario { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "O e-mail é obrigatório!")]
+        [EmailAddress(ErrorMessage = "O e-mail já existe!")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "A senha é obrigatória!")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "A senha deve conter de 5 á 20 caracteres!")]
+        public string Senha { get; set; }
+
+
+    }
+}
